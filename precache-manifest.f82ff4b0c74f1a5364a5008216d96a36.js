e9454bf4309d976cdbd4ffd622b296c689ef38e7
fenghsi  yu <fenghsi.yu@stonybrook.edu>
2020-01-08 02:30:30 +0800

    Updates

---
+++ precache-manifest.f82ff4b0c74f1a5364a5008216d96a36.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "306aadbc8831a0e846b425ae9257e88d",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "607048d816baa728d34d",
+    "url": "/my-app/static/css/2.35f98baf.chunk.css"
+  },
+  {
+    "revision": "db66a07ff16dde7cf4d8",
+    "url": "/my-app/static/css/main.b0fe6233.chunk.css"
+  },
+  {
+    "revision": "607048d816baa728d34d",
+    "url": "/my-app/static/js/2.b6080784.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/my-app/static/js/2.b6080784.chunk.js.LICENSE"
+  },
+  {
+    "revision": "db66a07ff16dde7cf4d8",
+    "url": "/my-app/static/js/main.bfc74fb2.chunk.js"
+  },
+  {
+    "revision": "16c42100ba74878e3357",
+    "url": "/my-app/static/js/runtime-main.97ffa0d1.js"
+  },
+  {
+    "revision": "97a974d272a7887aebb0f6b81d08fd0d",
+    "url": "/my-app/static/media/csg.97a974d2.png"
+  },
+  {
+    "revision": "f86c10d1430712de80e6010bb2a5ff29",
+    "url": "/my-app/static/media/e-comm.f86c10d1.png"
+  },
+  {
+    "revision": "78294cb06f3e737344c90312e54e0144",
+    "url": "/my-app/static/media/twitter.78294cb0.jpg"
+  }
+]);
